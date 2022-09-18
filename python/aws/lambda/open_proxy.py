9adc1d7cd8e27c187210ab86340a44dc61bf971b
mendel129 <mendel129@gmail.com>
2022-09-18 10:59:28 +0200

    Update open_proxy.py

--- python/aws/lambda/open_proxy.py
+++ python/aws/lambda/open_proxy.py
@@ -1,15 +1,16 @@
-#use aws lambda to proxy a request
+#use aws lambda to proxy an http request, not on purpose but for PoC get -> post
 #in this case to add cors headers to a website which doesnt has cors headers
 #todo: add authentication, an open proxy is never a good idea!
 #todo2: not sure if url needs to be from urlpath, maybe hardcode
+#todo3: convert to post -> post get data out of urlpath
 
 import json,urllib3,base64
 
 def lambda_handler(event, context):
     vara=event['queryStringParameters']['vara']
     varb=event['queryStringParameters']['varb']
-    url=event['queryStringParameters']['url']
     varc=event['queryStringParameters']['varc']
+    url=event['queryStringParameters']['url']
     
     payload=base64.b64encode((vara+":"+varb).encode('ascii')).decode('ascii')
     encoded_body="grant_type=password&username=idontexist&password="+varc
