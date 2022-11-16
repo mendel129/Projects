51e16766d2686bdb481207e1148042468d52abd5
mendel129 <mendel129@gmail.com>
2022-11-16 10:05:28 +0100

    Create check_bpost.py

---
+++ python/check_bpost.py
@@ -0,0 +1,12 @@
+import requests, json
+
+barcode=input("barcode: ")
+postcode=input("postcode: ")
+
+resp=requests.get("https://track.bpost.cloud/track/items?itemIdentifier="+barcode+"&postalCode="+postcode)
+# print(json.dumps(resp.json(), indent=3))
+jsondata=resp.json()
+print(jsondata["items"][0]["activeStep"]["label"]["main"]["EN"])
+
+for event in jsondata["items"][0]["events"]:
+    print("---- " + str(event["date"]) + " - " + str(event["time"]) + " - " + str(event["key"]["EN"]["description"]))
